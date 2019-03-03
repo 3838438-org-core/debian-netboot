582c5bf146707a8dd249464fa27627bbc842afe3
b <b@okj8@outlook.com>
2019-03-03 01:13:16 +0000

    ok

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
+sudo cp grub /etc/default/grub
 sudo apt update && sudo apt -y install ca-certificates whois
-sudo sh netboot.sh -c CN -u b -p b -ip 192.168.0.111 -cidr 255.255.255.0 -gw 192.168.0.1 -ns 8.8.8.8 8.8.4.4 -add git openssh whois ca-certificates openssl -dry-run
+sudo sh netboot.sh -c CN -u b -p b -ip 192.168.0.111 -cidr 255.255.255.0 -gw 192.168.0.1 -ns 8.8.8.8 8.8.4.4 -add "git openssh whois ca-certificates openssl" -dry-run
